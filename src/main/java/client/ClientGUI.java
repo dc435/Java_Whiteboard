e646a14f6fa58d822b5ba7dd7290a2396c563cd2
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-10-16 18:44:08 +1100

    Merging existing YC GUI

--- src/main/java/client/ClientGUI.java
+++ src/main/java/client/ClientGUI.java
@@ -3,6 +3,14 @@ package client;
 import message.NewWBRequest;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.net.InetSocketAddress;
 
 
@@ -13,7 +21,160 @@ public class ClientGUI extends JFrame {
     public ClientGUI(InetSocketAddress serverAddress) {
         this.serverAddress = serverAddress;
     }
+    private JPanel panel1;
+    private JPanel canvas;
+    private JButton lineButton;
+    private JButton circleButton;
+    private JButton rectangleButton;
+    private JButton freeButton;
+    private String brush;
+    public ArrayList<Shape> graphicsArrayList = new ArrayList<Shape>();
+    private Point2D.Float p1;
+    private Point2D.Float p2;
 
+
+    public ClientGUI(String appName) {
+        // From tutorial
+        super(appName);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.setBackground(Color.WHITE);
+        this.pack();
+
+        // Set size
+        this.setBounds(30,30,900,650);
+
+        // MouseListener for switching brushes
+        rectangleButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                brush = rectangleButton.getText();
+                System.out.println(brush); //debug
+            }
+        });
+
+        lineButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                brush = lineButton.getText();
+                System.out.println(brush); //debug
+            }
+        });
+
+        circleButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                brush = circleButton.getText();
+                System.out.println(brush); //debug
+            }
+        });
+
+        freeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                brush = freeButton.getText();
+                System.out.println(brush); //debug
+            }
+        });
+
+
+
+        // MouseListener for drawing on canvas
+        canvas.addMouseListener(new MouseAdapter() {
+
+            // Starting point of the shape
+            @Override
+            public void mousePressed(MouseEvent e) {
+                p1 = new Point2D.Float(e.getX(), e.getY());
+            }
+
+            // Ending point of the shape
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                p2 = new Point2D.Float(e.getX(), e.getY());
+
+                switch (brush) { //FIXME: weird error coming out
+
+                    case "Line":
+                        Line2D.Float line2D = new Line2D.Float(p1, p2);
+                        graphicsArrayList.add(line2D);
+                        repaint(); // Call paint(g)
+                        // TODO: Send out arraylist
+                        break;
+
+                    case "Circle":
+                        float x = (float) p1.getX();
+                        float y = (float) p1.getY();
+                        float w = (float) (p2.getX() - p1.getX());
+                        float h = (float) (p2.getY() - p1.getY());
+                        Ellipse2D.Float circle2D = new Ellipse2D.Float(x, y, w, h);
+                        graphicsArrayList.add(circle2D);
+                        repaint();
+                        // TODO: Send out arraylist
+                        break;
+
+                    case "Rectangle":
+                        float x1 = (float) p1.getX();
+                        float y1 = (float) p1.getY();
+                        float w1 = (float) (p2.getX() - p1.getX());
+                        float h1 = (float) (p2.getY() - p1.getY());
+                        Rectangle2D.Float rectangle2D = new Rectangle2D.Float(x1, y1, w1, h1);
+                        graphicsArrayList.add(rectangle2D);
+                        repaint();
+                        // TODO: Send out arraylist
+                        break;
+
+                }
+            }
+        });
+
+
+        canvas.addMouseMotionListener(new MouseAdapter() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+
+                if (brush.equals("FreeH")) { //FIXME: need to init p1 and p2 and clear positions
+
+                    p1.x = p2.x;
+                    p1.y = p2.y;
+                    p2.setLocation(e.getX(), e.getY());
+                    Line2D.Float line2D = new Line2D.Float(p1, p2);
+                    graphicsArrayList.add(line2D);
+                    repaint(); // Call paint(g)
+
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        ClientGUI frame = new ClientGUI("Tester");
+        frame.setVisible(true);
+
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        // Convert graphics objects to graphics2D objects
+        Graphics2D g2 = (Graphics2D) g;
+
+
+
+        // Iterate the lines array and draw
+        for (Shape obj : graphicsArrayList) {
+            g2.draw(obj);
+        }
+
+    }
+
+    public ArrayList<Shape> getGraphicsArrayList() {return this.graphicsArrayList;}
 
 
 
