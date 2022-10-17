01a65bccfaecfafbbb7da1aca59b03af2671ff58
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-10-18 00:51:06 +1100

    ShapeWrapper_v1

--- src/main/java/whiteboard/ShapeWrapper.java
+++ src/main/java/whiteboard/ShapeWrapper.java
@@ -1,5 +1,7 @@
 package whiteboard;
 
+import java.awt.*;
+
 public class ShapeWrapper {
 
     //TODO: YP
@@ -7,4 +9,24 @@ public class ShapeWrapper {
     //String color?
     //TextBox text?
 
+    private Shape shape;
+    private String color;
+    private String textPane; //FIXME: not implemented
+
+    // Getters and setters
+    public Shape getShape() {return this.shape;}
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public String getColor() {return this.color;}
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // FIXME: I haven't figured out how to add textPane on canvas
+    public String getTextPane() {return this.textPane;}
+    public void setTextPane(String textPane) {
+        this.textPane = textPane;
+    }
 }
