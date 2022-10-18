0e8b3070abde4c9cab4da1c8058124ccbb05dfab
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-10-18 19:11:16 +1100

    feat:update wrapper

--- src/main/java/whiteboard/ShapeWrapper.java
+++ src/main/java/whiteboard/ShapeWrapper.java
@@ -5,23 +5,22 @@ import java.awt.*;
 public class ShapeWrapper {
 
     //TODO: YP
-    private String brush;
     private Shape shape;
     private String color;
     private String textPane; //FIXME: not implemented
 
 
     // Constructor
-    public ShapeWrapper(Shape shape, String color, String brush) {
+    public ShapeWrapper(Shape shape, String color) {
         this.shape = shape;
         this.color = color;
     }
 
     // Getters and setters
-    public String getBrush() {return this.brush;}
-    public void setBrush(String brush) {
-        this.brush = brush;
-    }
+    //public String getBrush() {return this.brush;}
+    //public void setBrush(String brush) {
+        //this.brush = brush;
+    //}
     public Shape getShape() {return this.shape;}
     public void setShape(Shape shape) {
         this.shape = shape;
